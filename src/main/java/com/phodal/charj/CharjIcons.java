17920b5a4515922e3bf7c979c2999541808838c8
Phodal Huang <h@phodal.com>
2020-09-19 15:18:26 +0800

    feat: add ledge logo as file type icon

--- src/main/java/com/phodal/charj/CharjIcons.java
+++ src/main/java/com/phodal/charj/CharjIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class CharjIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/jar-gray.png");
+    public static final Icon FILE = IconLoader.getIcon("/icons/ledge.svg");
 }
