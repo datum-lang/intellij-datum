1c99fe32edc99d14d4107d84d2032f43d542aefb
Phodal Huang <h@phodal.com>
2020-09-22 07:37:02 +0800

    feat: init base compiletion provider

---
+++ src/main/java/com/phodal/charj/completion/CharjCompletionProvider.java
@@ -0,0 +1,10 @@
+package com.phodal.charj.completion;
+
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionProvider;
+import com.intellij.patterns.ElementPattern;
+import com.intellij.psi.PsiElement;
+
+abstract class CharjCompletionProvider  extends CompletionProvider<CompletionParameters> {
+//    ElementPattern<PsiElement> elementPattern;
+}
