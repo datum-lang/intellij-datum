a439d3f60802df67945ca2c85c070319bc44801f
Phodal Huang <h@phodal.com>
2020-10-30 13:03:02 +0800

    refactor: update lang in highlight

--- src/main/java/com/phodal/charj/lexer/CharjTokenTypeSets.java
+++ src/main/java/com/phodal/charj/lexer/CharjTokenTypeSets.java
@@ -5,20 +5,32 @@ import com.intellij.psi.tree.TokenSet;
 public interface CharjTokenTypeSets {
     TokenSet KEY_WORDS = TokenSet
             .create(
-                    CharjTypes.DOLLAR,
+                    CharjTypes.STRUCT_KEYWORD,
+                    CharjTypes.PKG_KEYWORD,
+                    CharjTypes.PACKAGE_KEYWORD,
+                    CharjTypes.IMPORT_KEYWORD,
+                    CharjTypes.AS_KEYWORD,
+                    CharjTypes.IF_KEYWORD,
+                    CharjTypes.ELSE_KEYWORD,
+                    CharjTypes.FUN_KEYWORD,
+                    CharjTypes.WHILE_KEYWORD,
+                    CharjTypes.FOR_KEYWORD,
+                    CharjTypes.BREAK_KEYWORD,
+                    CharjTypes.CONTINUE_KEYWORD,
+                    CharjTypes.RETURN_KEYWORD,
+                    CharjTypes.INT_KEYWORD,
+                    CharjTypes.FLOAT_KEYWORD,
+                    CharjTypes.STRING_KEYWORD,
                     CharjTypes.IN_KEYWORD,
                     CharjTypes.OUT_KEYWORD,
-                    CharjTypes.DEF_KEYWORD,
-                    CharjTypes.IMPORT_KEYWORD,
-                    CharjTypes.PACKAGE_KEYWORD,
-                    CharjTypes.MEMBER_PLACEHOLDER,
-                    CharjTypes.STRUCT_KEYWORD,
+                    CharjTypes.MEMBER_KEYWORD,
+                    CharjTypes.IN_KEYWORD,
+                    CharjTypes.INSTANCE_KEYWORD,
+                    CharjTypes.MATCH_KEYWORD,
                     CharjTypes.FX_KEYWORD,
-                    CharjTypes.IF_KEYWORD,
                     CharjTypes.ELIF_KEYWORD,
-                    CharjTypes.ELSE_KEYWORD,
-                    CharjTypes.END_KEYWORD,
-                    CharjTypes.THEN_KEYWORD
+                    CharjTypes.THEN_KEYWORD,
+                    CharjTypes.END_KEYWORD
             );
 
 }
