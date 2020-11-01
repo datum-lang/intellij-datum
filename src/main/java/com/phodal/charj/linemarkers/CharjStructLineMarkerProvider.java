b235de777dc8757f7b65c5e639b6974911f1f3ee
Phodal Huang <h@phodal.com>
2020-11-01 20:28:00 +0800

    feat: make first line marker for execute

--- src/main/java/com/phodal/charj/linemarkers/CharjStructLineMarkerProvider.java
+++ src/main/java/com/phodal/charj/linemarkers/CharjStructLineMarkerProvider.java
@@ -2,20 +2,45 @@ package com.phodal.charj.linemarkers;
 
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.icons.AllIcons;
 import com.intellij.psi.PsiElement;
+import com.phodal.charj.psi.CharjFunctionDefineName;
+import com.phodal.charj.psi.CharjStructNameDeclaration;
 import com.phodal.charj.psi.impl.CharjStructMethodDeclarationImpl;
 import org.jetbrains.annotations.NotNull;
 
-public class CharjStructLineMarkerProvider implements LineMarkerProvider {
+import java.util.Collection;
+
+public class CharjStructLineMarkerProvider extends RelatedItemLineMarkerProvider {
     @Override
-    public LineMarkerInfo<?> getLineMarkerInfo(@NotNull PsiElement element) {
+    protected void collectNavigationMarkers(@NotNull PsiElement element,
+                                            @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result) {
         // This must be an element with a literal expression as a parent
         System.out.println(element);
 
         if(!(element instanceof CharjStructMethodDeclarationImpl)) {
-            return null;
+            return;
         }
 
-        return null;
+        CharjStructNameDeclaration nameDecl = ((CharjStructMethodDeclarationImpl) element).getStructNameDeclaration();
+        String structName = nameDecl.getText();
+
+        CharjFunctionDefineName defineName = ((CharjStructMethodDeclarationImpl) element).getFunctionDefineName();
+        String funcName = defineName.getText();
+
+        if (structName.equals("default")  && funcName.equals("main")) {
+            // all icons: https://jetbrains.design/intellij/resources/icons_list/
+            NavigationGutterIconBuilder<PsiElement> builder =
+                    NavigationGutterIconBuilder.create(AllIcons.Actions.Execute)
+                            .setTargets(element)
+                            .setTooltipText("Run App");
+
+            result.add(builder.createLineMarkerInfo(element));
+        }
+
+        return;
     }
 }
