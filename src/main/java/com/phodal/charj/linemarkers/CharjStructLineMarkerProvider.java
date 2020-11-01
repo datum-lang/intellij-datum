36d04ab10df66c3302331320f87438308e9a4d75
Phodal Huang <h@phodal.com>
2020-11-01 20:14:28 +0800

    feat: init line marker

--- src/main/java/com/phodal/charj/linemarkers/CharjStructLineMarkerProvider.java
+++ src/main/java/com/phodal/charj/linemarkers/CharjStructLineMarkerProvider.java
@@ -3,11 +3,19 @@ package com.phodal.charj.linemarkers;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProvider;
 import com.intellij.psi.PsiElement;
+import com.phodal.charj.psi.impl.CharjStructMethodDeclarationImpl;
 import org.jetbrains.annotations.NotNull;
 
 public class CharjStructLineMarkerProvider implements LineMarkerProvider {
     @Override
     public LineMarkerInfo<?> getLineMarkerInfo(@NotNull PsiElement element) {
+        // This must be an element with a literal expression as a parent
+        System.out.println(element);
+
+        if(!(element instanceof CharjStructMethodDeclarationImpl)) {
+            return null;
+        }
+
         return null;
     }
 }
